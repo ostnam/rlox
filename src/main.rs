1550d47c1e04ff7737408243d8753e1e7116d21e
ostnam <tsougaevm@gmail.com>
2023-08-02 12:28:43 +0200

    basic main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, about)]
+struct CliArgs {
+    path: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+    match args.path {
+        None => repl(),
+        Some(path) => run_file(path),
+    }
+}
+
+fn repl() {
+    let stdin = io::stdin();
+    let mut buffer = String::new();
+    loop {
+        print!("> ");
+        let res = stdin.read_line(&mut buffer);
+        if let Err(e) = res {
+            println!("");
+            panic!("Error reading input line: {}", e);
+        }
+
+        interpret(buffer);
+    }
+}
+
+fn run_file(path: String) {
+    let src = match std::fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(e) => panic!("Error reading source file at: {}, error: {}", path, e),
+    };
 }
